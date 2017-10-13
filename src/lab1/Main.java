faac4df8f48301c8df621f36c0b472dbc4024358
shabab477 <shababkarim93@gmail.com>
2017-10-13 18:51:30 +0600

    added dda algo

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -38,7 +38,7 @@ public class Main {
         //This is where we put graphics event listener. 
         //Implement the Event Listener class to use a more object
         //oriented approach
-        glcanvas.addGLEventListener(new FileReaderListener());
+        glcanvas.addGLEventListener(new RandomDDA());
         
         glcanvas.setSize(SIZE_X, SIZE_Y);
         JFrame frame = new JFrame("My GL canvas");
@@ -49,6 +49,124 @@ public class Main {
         frame.setSize(SIZE_X, SIZE_Y);
         frame.setVisible(true);
     }
+    
+    static class Point
+    {
+        public float x1;
+        public float y1;
+        public float x2;
+        public float y2;
+
+        public Point(float x1, float y1, float x2, float y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+
+            if(x1 > x2)
+            {
+                //Swapping
+                float temp = x1;
+                x1 = x2;
+                x2 = temp;
+
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
+            }
+        }
+
+        public void draw(GL2 gl)
+        {
+            //System.out.println("calling");
+
+            float delta = (y2 - y1)/(x2 - x1);
+            if(delta >= -1 && delta <= 1)
+            {
+                while(x1 <= x2)
+                {
+                    gl.glVertex2d(x1, y1);
+                    y1 += (delta / 0.00001f);
+                    x1 += 0.00001f;
+                }
+            }
+            else
+            {
+                while(y1 <= y2)
+                {
+                    gl.glVertex2d(x1, y1);
+                    x1 += (1/(delta / 0.00001f));
+                    y1 += 0.00001f;
+                }
+            }
+        }
+    }
+	
+    static class RandomDDA implements GLEventListener
+    {
+
+        @Override
+        public void display(GLAutoDrawable drawable) {
+            GL2 gl = drawable.getGL().getGL2();
+            gl.glBegin(GL2.GL_POINTS);
+            
+            BufferedReader reader = null;
+            try
+            {
+                reader = new BufferedReader(new FileReader(new File("random")));
+                
+                String line = null;
+                while((line = reader.readLine()) != null)
+                {
+                    String div[] = line.split("\\s+");
+                    
+                    float x1 = Float.valueOf(div[0]) / SIZE_X;
+                    float x2 = Float.valueOf(div[2]) / SIZE_X;
+                    float y1 = Float.valueOf(div[1]) / SIZE_Y;
+                    float y2 = Float.valueOf(div[3]) / SIZE_Y;
+
+                    Point p = new Point(x1, y1, x2, y2);
+                    p.draw(gl);
+                }
+                
+            }catch(Exception e)
+            {
+                System.out.println(e.toString());
+            }
+            finally
+            {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                gl.glEnd();
+            }
+
+        }
+
+        @Override
+        public void dispose(GLAutoDrawable arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void init(GLAutoDrawable arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
+                        int arg4) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
     
     static class RandomPointsListener implements GLEventListener
     {
